22d09a384cc5562d51deb2b1933a9893f0224f34
Andrii Rykal <andreuka79@gmail.com>
2025-02-14 16:57:19 +0200

    test deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/sillems',
+  assetPrefix: '/sillems',
 };
 
 export default nextConfig;
