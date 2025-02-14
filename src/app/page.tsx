7ee300ab96a0a3a2d9c73db8509e1840d0a3397e
Andrii Rykal <andreuka79@gmail.com>
2025-02-14 11:04:06 +0200

    add lint-staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-export default function Home      () {
+export default function Home() {
   return (
     <main>
-      <h1>
-        
-        
-        
-        Hello world</h1>
+      <h1>Hello world</h1>
     </main>
-  )
+  );
 }
