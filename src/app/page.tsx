521aa2b757cbd131918aa089b5ca4b6026dcd1f8
Andrii Rykal <andreuka79@gmail.com>
2025-02-14 10:38:56 +0200

    test prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+export default function Home      () {
   return (
     <main>
-      <h1>Hello world</h1>
+      <h1>
+        
+        
+        
+        Hello world</h1>
     </main>
-  );
+  )
 }
